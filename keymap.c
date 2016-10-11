2f4d5776b1c2266df1750523b8c017a1d9287536
Anthony Laibe <anthony@laibe.cc>
2016-10-11 17:33:24 +0100

    add nav shortcut

--- keymap.c
+++ keymap.c
@@ -15,7 +15,6 @@
 /* Layers */
 #define BASE 0
 #define GUI_MOVE 1
-#define SHT_MOVE 2
 
 /* Macros */
 enum {
@@ -24,11 +23,10 @@ enum {
   AP_SAFARI,
   AP_MESSAGE,
   AP_FINDER,
-  AP_ITUNES,
+  AP_SPOTIFY,
   AP_SLEEP,
   AP_SEQUEL,
-  AP_INTELLIJ,
-  AP_RAMBOX,
+  AP_SLACK,
 
   NP,
 
@@ -53,6 +51,10 @@ enum {
   CT_LBP,
   CT_RBP,
   CT_CLN,
+  CT_IRC_MUTT,
+  CT_PROJ,
+  CT_GIT_CLOSE,
+  CT_TMUX_ZOOM
 };
 /* Fn keys */
 
@@ -76,75 +78,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Base Layer
  *
  * ,-----------------------------------------------------.           ,-----------------------------------------------------.
- * |         ~ |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |  0   |           |
+ * |         ~ |   1  |   2  |   3  |   4  |   5  | C-n  |           | C-p  |   6  |   7  |   8  |   9  |  0   |           |
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
- * |           |   Q  |   W  | D/SFT|   F  |   K  |   (  |           |  )   |   J  |   U  | R/SFT|   L  |  '   | \         |
+ * |      C-c  |   Q  |   W  | D/SFT|   F  |   K  |   (  |           |  )   |   J  |   U  | R/SFT|   L  |  '   | \         |
  * |-----------+------+------+------+------+------|   [  |           |  ]   |------+------+------+------+------+-----------|
- * |  Next/Prev|   A  | S/ALT| E/CTL| T/GUI| G/gui|------|           |------| Y/Gui| N/GUI| I/CTL| O/ALT|  H   | = /       |
+ * |  Next/Prev|   A  | S/ALT| E/CTL| T/GUI| G/gui|------|           |------|   Y  | N/GUI| I/CTL| O/ALT|  H   | = /       |
  * |-----------+------+------+------+------+------|   {  |           |  }   |------+------+------+------+------+-----------|
- * | Play/Pause|   Z  |   X  |   C  |V/SHTL|   B  |      |           |      |   P  |   M  |   ,  |   .  |      | KC_VOLU   |
+ * | Play/Pause|   Z  |   X  |   C  |   V  |   B  |      |           |      |   P  |   M  |   ,  |   .  |  :   | KC_VOLU   |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
- *     | cut   | copy |paste |   :  |  /   |                                       |   -  |      |      |      |KC_VOLD|
+ *     | cut   | copy |paste | C-k  |  /   |                                       |   -  | C-j  |      |      |KC_VOLD|
  *     `-----------------------------------'                                       `-----------------------------------'
  *                                         ,-------------.           ,-------------.
- *                                         | esc  |      |           |      | Del  |
+ *                                         | esc  |proj  |           | tmux | Del  |
  *                                  ,------|------|------|           |------+------+------.
- *                                  |      |      |      |           | LEAD |      |      |
+ *                                  |      |      |mail  |           |      |      |      |
  *                                  |Backsp| tab  |------|           |------| Enter| Space|
- *                                  |      |      |gui   |           |      |      |      |
+ *                                  |      |      | git  |           |      |      |      |
  *                                  `--------------------'           `--------------------'
  */
 [BASE] = KEYMAP(
 // left hand
- KC_TILD          ,TD(KF_1)   ,TD(KF_2)   ,TD(KF_3)   ,TD(KF_4)         ,TD(KF_5),KC_NO
-,KC_NO            ,KC_Q       ,KC_W       ,SFT_T(KC_D),KC_F             ,KC_K    ,TD(CT_LBP)
+ KC_GRAVE         ,TD(KF_1)   ,TD(KF_2)   ,TD(KF_3)   ,TD(KF_4)         ,TD(KF_5),LCTL(KC_N)
+,LCTL(KC_C)       ,KC_Q       ,KC_W       ,SFT_T(KC_D),KC_F             ,KC_K    ,TD(CT_LBP)
 ,M(NP)            ,KC_A       ,ALT_T(KC_S),CTL_T(KC_E),GUI_T(KC_T)      ,LT(GUI_MOVE, KC_G)
-,KC_MPLY          ,KC_Z       ,KC_X       ,KC_C       ,LT(SHT_MOVE,KC_V),KC_B    ,KC_LCBR
-,LGUI(KC_X)       ,LGUI(KC_C) ,LGUI(KC_V) ,TD(CT_CLN) ,TD(KF_SLSH)
+,KC_MPLY          ,KC_Z       ,KC_X       ,KC_C       ,KC_V             ,KC_B    ,KC_LCBR
+,LGUI(KC_X)       ,LGUI(KC_C) ,LGUI(KC_V) ,LCTL(KC_K) ,TD(KF_SLSH)
 
-,KC_ESC ,KC_NO
-,KC_NO
-,KC_BSPC,KC_TAB,TG(GUI_MOVE)
+,KC_ESC ,TD(CT_PROJ)
+,TD(CT_IRC_MUTT)
+,KC_BSPC,KC_TAB,TD(CT_GIT_CLOSE)
 
 
 // right hand
-,KC_NO             ,TD(KF_6)   ,TD(KF_7)        ,TD(KF_8)   ,TD(KF_9)        ,TD(KF_0)   ,KC_NO
+,LCTL(KC_P)        ,TD(KF_6)   ,TD(KF_7)        ,TD(KF_8)   ,TD(KF_9)        ,TD(KF_0)   ,KC_NO
 ,TD(CT_RBP)        ,KC_J       ,KC_U            ,SFT_T(KC_R),KC_L            ,TD(KF_QUOT),KC_BSLS
-,LT(GUI_MOVE, KC_Y),GUI_T(KC_N),CTL_T(KC_I)     ,ALT_T(KC_O),KC_H            ,TD(KF_EQL)
-,KC_RCBR           ,KC_P       ,KC_M            ,TD(KF_COMM),TD(KF_DOT)      ,KC_NO      ,KC_VOLU
-,TD(KF_MINS)       ,KC_NO      ,KC_NO           ,KC_NO      ,KC_VOLD
+,KC_Y              ,GUI_T(KC_N),CTL_T(KC_I)     ,ALT_T(KC_O),KC_H            ,TD(KF_EQL)
+,KC_RCBR           ,KC_P       ,KC_M            ,TD(KF_COMM),TD(KF_DOT)      ,TD(CT_CLN) ,KC_VOLU
+,TD(KF_MINS)       ,LCTL(KC_J) ,KC_NO           ,KC_NO      ,KC_VOLD
 
-,KC_NO,KC_DEL
+,TD(CT_TMUX_ZOOM),KC_DEL
 ,KC_LEAD
 ,KC_NO   ,KC_ENT ,KC_SPC
     ),
 /* Keymap 1: Gui and move
  *
  * ,-----------------------------------------------------.           ,-----------------------------------------------------.
- * |           |      |      |      |      |      |      |           | sleep|iterm |inteli|safari|rambox|itunes| message   |
+ * |           |      |      |      |      |      |      |           | sleep|iterm |slack |safari|finder|spoti | message   |
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
- * |           | G_Q  | G_W  |      | G_R  | G_T  |      |           |      |      | home | pgdn | pgup |  end | sequel    |
+ * |           |      |      |      |      |      |      |           |      |      | home | pgdn | pgup |  end | sequel    |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           | G_A  | G_S  |      | G_F  | base |------|           |------| base | left | down | top  |right | finder    |
+ * |           |      |      |      |      | base |------|           |------|      | left | down | top  |right |           |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           | G_Z  | G_X  | G_C  | G_V  |      | G_{  |           | G_}  |      | wleft| wdn  | wup  |wrgt  |           |
+ * |           |      |      |      |      |      |      |           |      |      | wleft| wdn  | wup  |wrgt  |           |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
  *      |      |      |      |      |      |                                       |      |      |      |      |      |
- *      `----------------------------------'                                       `----------------------------------'
+k*      `----------------------------------'                                       `----------------------------------'
  *                                         ,-------------.           ,-------------.
  *                                         |      |      |           |      |      |
  *                                  ,------|------|------|           |------+------+------.
  *                                  |      |      |      |           |      |Left  |Right |
- *                                  | G_Spc|enter |------|           |------| Click| Click|
- *                                  |      |      | base |           |      |      |      |
+ *                                  |      |      |------|           |------| Click| Click|
+ *                                  |      |      |      |           |      |      |      |
  *                                  `--------------------'           `--------------------'
  */
 [GUI_MOVE] = KEYMAP(
 // left hand
  KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO     ,KC_NO
-,KC_NO      ,LGUI(KC_Q) ,LGUI(KC_W) ,KC_NO      ,LGUI(KC_R) ,LGUI(KC_T),KC_NO
-,KC_NO      ,LGUI(KC_A) ,LGUI(KC_S) ,KC_NO      ,LGUI(KC_F) ,KC_TRNS
-,KC_NO      ,LGUI(KC_Z) ,LGUI(KC_X) ,LGUI(KC_C) ,LGUI(KC_V) ,KC_NO     ,KC_NO
+,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO     ,KC_NO
+,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_TRNS
+,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO     ,KC_NO
 ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO
 
 ,KC_NO ,KC_NO
@@ -152,59 +154,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ,LGUI(KC_SPC),KC_ENT,KC_TRNS
 
 // right hand
-,M(AP_SLEEP),M(AP_ITERM),M(AP_INTELLIJ),M(AP_SAFARI),M(AP_RAMBOX),M(AP_ITUNES) ,M(AP_MESSAGE)
-,KC_NO      ,KC_NO      ,KC_HOME       ,KC_PGDN     ,KC_PGUP     ,KC_END       ,M(AP_SEQUEL)
+,M(AP_SLEEP),M(AP_ITERM),M(AP_SLACK)   ,M(AP_SAFARI),M(AP_FINDER),M(AP_SPOTIFY) ,M(AP_MESSAGE)
+,KC_NO      ,KC_NO      ,KC_HOME       ,KC_PGDN     ,KC_PGUP     ,KC_END        ,M(AP_SEQUEL)
 ,KC_TRNS    ,KC_LEFT    ,KC_DOWN       ,KC_UP       ,KC_RGHT     ,M(AP_FINDER)
-,KC_NO      ,KC_NO      ,LALT(KC_LEFT) ,KC_WH_D     ,KC_WH_U     ,LALT(KC_RGHT),KC_NO
+,KC_NO      ,KC_NO      ,LALT(KC_LEFT) ,KC_WH_D     ,KC_WH_U     ,LALT(KC_RGHT) ,KC_NO
 ,KC_NO      ,KC_NO      ,KC_NO         ,KC_NO       ,KC_NO
 
 ,KC_NO,KC_NO
 ,KC_NO
 ,KC_NO,KC_BTN1 ,KC_BTN2
-    ),
-/* Keymap 2: Shift and move
- *
- * ,-----------------------------------------------------.           ,-----------------------------------------------------.
- * |           |      |      |      |      |      |      |           |      |      |      |      |      |      |           |
- * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
- * |           |      |      |      |      |      |      |           |      |      | home | pgdn | pgup |  end |           |
- * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |      |      |      |      |      |------|           |------|      | left | down | top  |right |           |
- * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |      |      |      | base |      |      |           |      |      | wleft|      |      |wrgt  |           |
- * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
- *      |      |      |      |      |      |                                       |      |      |      |      |      |
- *      `----------------------------------'                                       `----------------------------------'
- *                                         ,-------------.           ,-------------.
- *                                         |      |      |           |      |      |
- *                                  ,------|------|------|           |------+------+------.
- *                                  |      |      |      |           |      |      |      |
- *                                  |      |      |------|           |------|      |      |
- *                                  |      |      | base |           |      |      |      |
- *                                  `--------------------'           `--------------------'
- */
-[SHT_MOVE] = KEYMAP(
-// left hand
- KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO     ,KC_NO
-,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO     ,KC_NO
-,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO
-,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_TRNS    ,KC_NO     ,KC_NO
-,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO      ,KC_NO
-
-,KC_NO ,KC_NO
-,KC_NO
-,KC_NO ,KC_NO,KC_TRNS
-
-// right hand
-,KC_NO      ,KC_NO        ,KC_NO               ,KC_NO         ,KC_NO         ,KC_NO              ,KC_NO
-,KC_NO      ,KC_NO        ,LSFT(KC_HOME)       ,LSFT(KC_PGDN) ,LSFT(KC_PGUP) ,LSFT(KC_END)       ,KC_NO
-,KC_NO      ,LSFT(KC_LEFT),LSFT(KC_DOWN)       ,LSFT(KC_UP)   ,LSFT(KC_RGHT) ,KC_NO
-,KC_NO      ,KC_NO        ,LSFT(LALT(KC_LEFT)) ,KC_NO         ,KC_NO         ,LSFT(LALT(KC_RGHT)),KC_NO
-,KC_NO      ,KC_NO        ,KC_NO               ,KC_NO         ,KC_NO
-
-,KC_NO,KC_NO
-,KC_NO
-,KC_NO,KC_NO,KC_NO
     ),
 };
 
@@ -272,14 +230,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       wait_ms(255);
       return MACRO(T(M), T(E), T(S), T(S), T(A), T(G), T(E), T(ENT), END);
     }
-  case AP_ITUNES:
+  case AP_SPOTIFY:
     if (record->event.pressed) {
       register_code (KC_LGUI);
       register_code (KC_SPC);
       unregister_code (KC_LGUI);
       unregister_code (KC_SPC);
       wait_ms(255);
-      return MACRO(T(I), T(T), T(U), T(N), T(E), T(S), T(ENT), END);
+      return MACRO(T(S), T(P), T(O), T(T), T(I), T(F), T(Y), T(ENT), END);
     }
   case AP_SLEEP:
     if (record->event.pressed) {
@@ -299,23 +257,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       wait_ms(255);
       return MACRO(T(S), T(E), T(Q), T(U), T(E), T(L), T(ENT), END);
     }
-  case AP_INTELLIJ:
+  case AP_SLACK:
     if (record->event.pressed) {
       register_code (KC_LGUI);
       register_code (KC_SPC);
       unregister_code (KC_LGUI);
       unregister_code (KC_SPC);
       wait_ms(255);
-      return MACRO(T(I), T(N), T(T), T(E), T(L), T(L), T(I), T(J), T(ENT), END);
-    }
-  case AP_RAMBOX:
-    if (record->event.pressed) {
-      register_code (KC_LGUI);
-      register_code (KC_SPC);
-      unregister_code (KC_LGUI);
-      unregister_code (KC_SPC);
-      wait_ms(255);
-      return MACRO(T(R), T(A), T(M), T(B), T(O), T(X), T(ENT), END);
+      return MACRO(T(S), T(L), T(A), T(C), T(K), T(ENT), END);
     }
   }
 
@@ -344,6 +293,85 @@ void matrix_init_user(void) {
 
 LEADER_EXTERNS();
 
+static void ang_tap_dance_irc_mutt (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_LCTRL);
+    unregister_code (KC_S);
+    register_code (KC_I);
+    unregister_code (KC_I);
+  } else {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_LCTRL);
+    unregister_code (KC_S);
+    register_code (KC_M);
+    unregister_code (KC_M);
+  }
+}
+
+static void ang_tap_dance_tmux_zoom (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_S);
+    unregister_code (KC_LCTRL);
+  } else {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_LCTRL);
+    unregister_code (KC_S);
+    register_code (KC_Z);
+    unregister_code (KC_Z);
+  }
+}
+
+static void ang_tap_dance_proj (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_S);
+    register_code (KC_J);
+    unregister_code (KC_J);
+    unregister_code (KC_LCTRL);
+  } else {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_S);
+    register_code (KC_T);
+    unregister_code (KC_T);
+    unregister_code (KC_LCTRL);
+  }
+}
+
+static void ang_tap_dance_git_close (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_LCTRL);
+    unregister_code (KC_S);
+    register_code (KC_G);
+    unregister_code (KC_G);
+  } else {
+    register_code (KC_LCTRL);
+    register_code (KC_S);
+    unregister_code (KC_LCTRL);
+    unregister_code (KC_S);
+    register_code (KC_X);
+    unregister_code (KC_X);
+  }
+}
+
+static void ang_tap_dance_bspc_bk (qk_tap_dance_state_t *state, void *user_data) {
+  register_code (KC_LCTRL);
+  register_code (KC_S);
+  unregister_code (KC_LCTRL);
+  unregister_code (KC_S);
+  register_code (KC_A);
+  unregister_code (KC_A);
+}
+
 qk_tap_dance_action_t tap_dance_actions[] = {
   [CT_CLN]        = ACTION_TAP_DANCE_DOUBLE (KC_COLN, KC_SCLN),
   [CT_LBP]        = ACTION_TAP_DANCE_DOUBLE (KC_LBRC, KC_LPRN),
@@ -364,6 +392,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [KF_SLSH]       = ACTION_TAP_DANCE_DOUBLE (KC_SLSH, LSFT(KC_SLSH)),
   [KF_MINS]       = ACTION_TAP_DANCE_DOUBLE (KC_MINS, KC_UNDS),
   [KF_EQL]        = ACTION_TAP_DANCE_DOUBLE (KC_EQL, KC_PLUS),
+  [CT_GIT_CLOSE]  = ACTION_TAP_DANCE_FN (ang_tap_dance_git_close),
+  [CT_IRC_MUTT]   = ACTION_TAP_DANCE_FN (ang_tap_dance_irc_mutt),
+  [CT_PROJ]       = ACTION_TAP_DANCE_FN (ang_tap_dance_proj),
+  [CT_TMUX_ZOOM]  = ACTION_TAP_DANCE_FN (ang_tap_dance_tmux_zoom),
 };
 
 // Runs constantly in the background, in a loop.
@@ -383,19 +415,13 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_on();
   }
 
-  if (layer == SHT_MOVE) {
-    ergodox_right_led_1_on();
-    ergodox_right_led_2_on();
-  }
-
   if (keyboard_report->mods & MOD_BIT(KC_LSFT) ||
       ((get_oneshot_mods() & MOD_BIT(KC_LSFT)) && !has_oneshot_mods_timed_out())) {
     ergodox_right_led_1_set (LED_BRIGHTNESS_HI);
     ergodox_right_led_1_on ();
   } else {
     ergodox_right_led_1_set (LED_BRIGHTNESS_LO);
-    if (layer != SHT_MOVE)
-      ergodox_right_led_1_off ();
+    ergodox_right_led_1_off ();
   }
 
   if (keyboard_report->mods & MOD_BIT(KC_LALT) ||
@@ -404,7 +430,7 @@ void matrix_scan_user(void) {
     ergodox_right_led_2_on ();
   } else {
     ergodox_right_led_2_set (LED_BRIGHTNESS_LO);
-    if (layer != GUI_MOVE && layer != SHT_MOVE)
+    if (layer != GUI_MOVE)
       ergodox_right_led_2_off ();
   }
 
